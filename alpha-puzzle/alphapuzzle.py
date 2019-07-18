a0311d0fba7f2f65312c3565648e9cbf72aa39ff
alexgooding <alexxgooding@gmail.com>
2019-07-18 23:04:05 +0100

    form script into class

--- alpha-puzzle/alphapuzzle.py
+++ alpha-puzzle/alphapuzzle.py
@@ -1,54 +1,63 @@
 import json
 
 
+class AlphaPuzzleSolver:
 
-def parse_puzzle(puzzle):
-    word = []
     h_word_list = []
     v_word_list = []
-    board = puzzle.get("board")
-    for row in board:
-        for index in row:
-            if index == 0 and len(word) > 1:
-                h_word_list.append(word)
+
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
+        self.board = puzzle.get("board")
+
+    def parse_board(self):
+        word = []
+        # Form horizontal words
+        for row in self.board:
+            for index in row:
+                if index == 0 and len(word) > 1:
+                    self.h_word_list.append(word)
+                    word = []
+                elif index == 0 and len(word) < 2:
+                    word = []
+                elif not index == 0:
+                    word.append(index)
+            if len(word) > 1:
+                self.h_word_list.append(word)
                 word = []
-            elif index == 0 and len(word) < 2:
+            elif len(word) < 2:
                 word = []
-            elif not index == 0:
-                word.append(index)
-        if len(word) > 1:
-            h_word_list.append(word)
-            word = []
-        elif len(word) < 2:
-            word = []
 
-    word = []
+        word = []
 
-    for column in range(len(board[0])):
-        for index in range(len(board)):
-            if board[index][column] == 0 and len(word) > 1:
-                v_word_list.append(word)
+        # Form vertical words
+        for column in range(len(self.board[0])):
+            for index in range(len(self.board)):
+                if self.board[index][column] == 0 and len(word) > 1:
+                    self.v_word_list.append(word)
+                    word = []
+                elif self.board[index][column] == 0 and len(word) < 2:
+                    word = []
+                elif not self.board[index][column] == 0:
+                    word.append(self.board[index][column])
+            if len(word) > 1:
+                self.v_word_list.append(word)
                 word = []
-            elif board[index][column] == 0 and len(word) < 2:
+            elif len(word) < 2:
                 word = []
-            elif not board[index][column] == 0:
-                word.append(board[index][column])
-        if len(word) > 1:
-            v_word_list.append(word)
-            word = []
-        elif len(word) < 2:
-            word = []
 
+        print(self.h_word_list)
+        print(self.v_word_list)
+    #
+    # def substitutor(self, puzzle):
+    #     for num in puzzle.get("letters"):
+    #         for
 
-    print(h_word_list)
-    print(v_word_list)
-#
-# def substitutor(puzzle):
-#     for num in puzzle.get("letters"):
-#         for
+    def word_search(self, partial_word):
+        possible_words = []
 
-#def solve(puzzle):
 if __name__ == "__main__":
     with open("day1.json", 'r') as f:
         day_1 = json.load(f)
-    parse_puzzle(day_1)
+    solver = AlphaPuzzleSolver(day_1)
+    solver.parse_board()
