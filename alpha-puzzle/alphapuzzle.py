a3f326232f8e5c7a6f56833b831c79778478de75
Alex Gooding <alexxgooding@gmail.com>
2019-07-20 03:41:28 +0100

    full brute force solver with errors. Last added a check for existing characters in letter

--- alpha-puzzle/alphapuzzle.py
+++ alpha-puzzle/alphapuzzle.py
@@ -1,5 +1,6 @@
 import json
 import re
+import copy
 
 
 class AlphaPuzzleSolver:
@@ -8,6 +9,7 @@ class AlphaPuzzleSolver:
     v_word_list = []
 
     def __init__(self, puzzle, all_words):
+        self.original_puzzle = copy.deepcopy(puzzle)
         self.puzzle = puzzle
         self.board = puzzle.get("board")
         self.letters = puzzle.get("letters")
@@ -50,42 +52,103 @@ class AlphaPuzzleSolver:
                 word = []
 
     def solve_board(self):
-        while len(self.letters) < 26:
-            self.substitutor()
-            for partial_word in self.h_word_list:
-                self.word_search(partial_word)
-            for partial_word in self.v_word_list:
-                self.word_search(partial_word)
+        h_possible_words = []
+        v_possible_words = []
+        self.substitutor()
+        h_possible_words = self.find_all_possible_words(self.h_word_list)
+        v_possible_words = self.find_all_possible_words(self.v_word_list)
+        h_possible_words.sort(key=len)
+        v_possible_words.sort(key=len)
+        print(h_possible_words)
+        print(v_possible_words)
+        while True:
+            word_guess = ""
+            if len(h_possible_words[0]) == 1:
+                del h_possible_words[0]
+            if len(v_possible_words[0]) == 1:
+                del v_possible_words[0]
+            if len(h_possible_words[0]) <= len(v_possible_words[0]):
+                word_guess = h_possible_words[0][1]
+                self.update_letters(word_guess, self.h_word_list[int(h_possible_words[0][0])])
+            else:
+                word_guess = v_possible_words[0][1]
+                self.update_letters(word_guess, self.v_word_list[int(v_possible_words[0][0])])
+            print(word_guess)
+            h_match_flag = True
+            v_match_flag = True
+            while h_match_flag and v_match_flag:
+                if len(self.letters) == 26:
+                    return self.puzzle
+                self.substitutor()
+                h_match_flag = self.find_all_possible_words(self.h_word_list)
+                v_match_flag = self.find_all_possible_words(self.v_word_list)
 
-        self.print_word_lists()
+                self.print_word_lists()
+
+            if len(h_possible_words[0]) <= len(v_possible_words[0]):
+                del h_possible_words[0][1]
+            else:
+                del v_possible_words[0][1]
+            self.puzzle = copy.deepcopy(self.original_puzzle)
+            self.board = self.puzzle.get("board")
+            self.letters = self.puzzle.get("letters")
+            self.h_word_list = []
+            self.v_word_list = []
+            self.substitutor()
+
+    def find_all_possible_words(self, word_list):
+        all_possible_words = []
+        for index, partial_word in enumerate(word_list):
+            possible_words = self.word_search(str(index), partial_word)
+            if possible_words:
+                all_possible_words.append(possible_words)
+        return all_possible_words
 
     def substitutor(self):
-        for word in self.h_word_list:
-            for char in word:
+        for word_index in range(len(self.h_word_list)):
+            for char_index in range(len(self.h_word_list[word_index])):
                 for num, letter in self.letters.items():
-                    if char == num:
-                        char = letter
-        for word in self.v_word_list:
-            for char in word:
+                    if self.h_word_list[word_index][char_index] == num:
+                        self.h_word_list[word_index][char_index] = letter.lower()
+        for word_index in range(len(self.v_word_list)):
+            for char_index in range(len(self.v_word_list[word_index])):
                 for num, letter in self.letters.items():
-                    if char == num:
-                        char = letter
+                    if self.v_word_list[word_index][char_index] == num:
+                        self.v_word_list[word_index][char_index] = letter.lower()
 
 
-    def word_search(self, partial_word):
+    def word_search(self, index, partial_word):
         # Find pairs of matching characters
         char_pairs = self.find_pairs(partial_word)
         regex = self.create_regex(partial_word)
         # search for all words using regex
         possible_words = self.search_dictionary(regex)
-        for word in possible_words:
+        for word in possible_words[:]:
+            word_not_removed = True
             for pair in char_pairs:
                 if word[pair[0]] != word[pair[1]]:
                     possible_words.remove(word)
+                    word_not_removed = False
+                    break
+            if word_not_removed:
+                for char_index in range(len(partial_word)):
+                    if partial_word[char_index].isdigit():
+                        if word[char_index] in self.letters.values():
+                            possible_words.remove(word)
+                            break
+
+
+
+       # print(possible_words)
 
         if len(possible_words) == 1:
             self.update_letters(possible_words[0], partial_word)
-            # partial_word = list(possible_words[0])
+            return possible_words.insert(0, index)
+        elif len(possible_words) == 0:
+            return possible_words
+        else:
+            possible_words.insert(0, index)
+            return possible_words
 
     def find_pairs(self, partial_word):
         char_pairs = []
@@ -110,21 +173,25 @@ class AlphaPuzzleSolver:
     def search_dictionary(self, regex):
         matches = []
         for s in self.all_words:
-            match = re.search(regex, s)
+            match = re.search(regex, s, re.IGNORECASE)
             if match:
-                matches.append(match)
+                matches.append(match.group(0))
         return matches
 
     def update_letters(self, complete_word, partial_word):
-        for i in range(partial_word):
+        for i in range(len(partial_word)):
             if partial_word[i].isdigit():
                 new_entry = {partial_word[i]: complete_word[i]}
                 self.letters.update(new_entry)
 
     def print_word_lists(self):
+        # print(self.h_word_list)
+        # print(self.v_word_list)
+        #print(self.letters)
+        print(len(self.letters))
         print(self.h_word_list)
         print(self.v_word_list)
-        # print(self.all_words)
+        print()
 
 if __name__ == "__main__":
     all_words = []
@@ -132,7 +199,10 @@ if __name__ == "__main__":
         day_1 = json.load(f1)
     with open("all_words.txt", 'r') as f2:
         for line in f2:
-            all_words.append(line)
+            all_words.append(line.lower())
     solver = AlphaPuzzleSolver(day_1, all_words)
     solver.parse_board()
-    solver.solve_board()
+    print(solver.solve_board())
+    # solver.substitutor()
+    # print(solver.v_word_list[-1])
+    # solver.word_search(solver.v_word_list[-1])
