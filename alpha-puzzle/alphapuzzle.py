fa1178e0c79aad79013921fd409f11a69066fa91
Alex Gooding <alexxgooding@gmail.com>
2019-07-20 13:32:18 +0100

    solves day1 but without nice output

--- alpha-puzzle/alphapuzzle.py
+++ alpha-puzzle/alphapuzzle.py
@@ -1,18 +1,24 @@
 import json
 import re
 import copy
+import string
 
 
 class AlphaPuzzleSolver:
 
     h_word_list = []
     v_word_list = []
+    h_word_list_original = []
+    v_word_list_original = []
+    not_found = list(string.ascii_lowercase)
 
     def __init__(self, puzzle, all_words):
         self.original_puzzle = copy.deepcopy(puzzle)
         self.puzzle = puzzle
         self.board = puzzle.get("board")
-        self.letters = puzzle.get("letters")
+        self.letters = dict((k, v.lower()) for k, v in puzzle.get("letters").items())
+        for char in self.letters.values():
+            self.not_found.remove(char.lower())
         self.all_words = all_words
 
     def parse_board(self):
@@ -51,6 +57,9 @@ class AlphaPuzzleSolver:
             elif len(word) < 2:
                 word = []
 
+        self.h_word_list_original = copy.deepcopy(self.h_word_list)
+        self.v_word_list_original = copy.deepcopy(self.v_word_list)
+
     def solve_board(self):
         h_possible_words = []
         v_possible_words = []
@@ -76,15 +85,22 @@ class AlphaPuzzleSolver:
             print(word_guess)
             h_match_flag = True
             v_match_flag = True
-            while h_match_flag and v_match_flag:
-                if len(self.letters) == 26:
-                    return self.puzzle
+            while h_match_flag or v_match_flag:
                 self.substitutor()
+                # Remove new found letters before forming regex
+                for char in self.letters.values():
+                    if char.lower() in self.not_found:
+                        self.not_found.remove(char.lower())
                 h_match_flag = self.find_all_possible_words(self.h_word_list)
                 v_match_flag = self.find_all_possible_words(self.v_word_list)
 
+
+
                 self.print_word_lists()
 
+            if len(self.letters) == 26:
+                self.create_json_solution()
+                return
             if len(h_possible_words[0]) <= len(v_possible_words[0]):
                 del h_possible_words[0][1]
             else:
@@ -92,8 +108,8 @@ class AlphaPuzzleSolver:
             self.puzzle = copy.deepcopy(self.original_puzzle)
             self.board = self.puzzle.get("board")
             self.letters = self.puzzle.get("letters")
-            self.h_word_list = []
-            self.v_word_list = []
+            self.h_word_list = copy.deepcopy(self.h_word_list_original)
+            self.v_word_list = copy.deepcopy(self.v_word_list_original)
             self.substitutor()
 
     def find_all_possible_words(self, word_list):
@@ -124,20 +140,10 @@ class AlphaPuzzleSolver:
         # search for all words using regex
         possible_words = self.search_dictionary(regex)
         for word in possible_words[:]:
-            word_not_removed = True
             for pair in char_pairs:
                 if word[pair[0]] != word[pair[1]]:
                     possible_words.remove(word)
-                    word_not_removed = False
                     break
-            if word_not_removed:
-                for char_index in range(len(partial_word)):
-                    if partial_word[char_index].isdigit():
-                        if word[char_index] in self.letters.values():
-                            possible_words.remove(word)
-                            break
-
-
 
        # print(possible_words)
 
@@ -166,7 +172,7 @@ class AlphaPuzzleSolver:
             if char.isalpha():
                 regex += char
             else:
-                regex += "."
+                regex += "[" + ",".join(self.not_found) + "]"
         regex += "$"
         return regex
 
@@ -179,10 +185,14 @@ class AlphaPuzzleSolver:
         return matches
 
     def update_letters(self, complete_word, partial_word):
+        found_letters = self.letters.values()
         for i in range(len(partial_word)):
             if partial_word[i].isdigit():
                 new_entry = {partial_word[i]: complete_word[i]}
                 self.letters.update(new_entry)
+                if complete_word[i] not in found_letters:
+                    self.not_found.remove(complete_word[i])
+                    found_letters.append(complete_word[i])
 
     def print_word_lists(self):
         # print(self.h_word_list)
@@ -193,6 +203,15 @@ class AlphaPuzzleSolver:
         print(self.v_word_list)
         print()
 
+    def create_json_solution(self):
+        for word_index in range(len(self.board)):
+            for char_index in range(len(self.board[word_index])):
+                if str(self.board[word_index][char_index]) in self.letters.keys():
+                    self.board[word_index][char_index] = self.letters.get(str(self.board[word_index][char_index]))
+
+        with open('day1_solution.json', 'w') as outfile:
+            json.dump(self.puzzle, outfile, indent=4, sort_keys=True)
+
 if __name__ == "__main__":
     all_words = []
     with open("day1.json", 'r') as f1:
@@ -202,7 +221,8 @@ if __name__ == "__main__":
             all_words.append(line.lower())
     solver = AlphaPuzzleSolver(day_1, all_words)
     solver.parse_board()
-    print(solver.solve_board())
+    solver.solve_board()
+    print(solver.letters)
     # solver.substitutor()
     # print(solver.v_word_list[-1])
     # solver.word_search(solver.v_word_list[-1])
